7534cab478e522d1829b3a1874d3aed48fb96767
dragon6810 <35243479+dragon6810@users.noreply.github.com>
2025-08-25 09:02:46 -0400

    ftree construction

--- src/ftree.c
+++ src/ftree.c
@@ -25,8 +25,42 @@ void ftree_free(ftree_t* tree)
     }
 }
 
+static ftree_child_t* ftree_findchild(ftree_t* node, const char* name, ftree_childtype_e type)
+{
+    ftree_child_t* child;
+
+    assert(node);
+    assert(name);
+
+    child = node->children;
+    while(child)
+    {
+        if(!strcmp(child->name, name))
+            return child;
+        child = child->next;
+    }
+
+    child = malloc(sizeof(ftree_child_t));
+    memset(child, 0, sizeof(ftree_child_t));
+    strcpy(child->name, name);
+    child->type = type;
+    child->next = node->children;
+    node->children = child;
+
+    if(type == FTREE_CHILD_TREE)
+    {
+        child->node = malloc(sizeof(ftree_t));
+        memset(child->node, 0, sizeof(ftree_t));
+        strcpy(child->node->path, name);
+    }
+
+    return child;
+}
+
 void ftree_loadpath(ftree_t* tree, const char* path)
 {
+    ftree_child_t *newchild;
+    ftree_t *curnode;
     char relative[PATH_MAX], slice[PATH_MAX];
     char *start, *end;
 
@@ -41,6 +75,7 @@ void ftree_loadpath(ftree_t* tree, const char* path)
 
     pathrelativetorepo(path, relative);
 
+    curnode = tree;
     start = relative;
     while(*start)
     {
@@ -51,10 +86,18 @@ void ftree_loadpath(ftree_t* tree, const char* path)
         memcpy(slice, start, end - start);
         slice[end - start] = 0;
 
-        printf("slice: %s\n", slice);
-
         if(!*end)
-            break;
+        {
+            printf("adding blob %s\n", slice);
+            ftree_findchild(curnode, slice, FTREE_CHILD_BLOB);
+            return;
+        }
+
+        printf("adding tree %s\n", slice);
+
+        newchild = ftree_findchild(curnode, slice, FTREE_CHILD_TREE);
+        curnode = newchild->node;
+
         start = end + 1;
     }
 }
@@ -72,7 +115,7 @@ void ftree_loadindex(ftree_t* outftree)
     index_prune();
 
     memset(&idx, 0, sizeof(index_t));
-    
+
     index_load(&idx);
 
     cur = idx.entries;
